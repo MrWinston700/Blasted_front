8557b0d15ae704681f98d7a900f3702322b3c46f
MrWinston700 <onestarrecruit@gmail.com>
2021-12-15 15:34:00 -0500

    style: started work on fleet section: dividing sections

--- blasted-front/src/components/fleet.js
+++ blasted-front/src/components/fleet.js
@@ -5,6 +5,42 @@ const Fleet = () => {
     <div id="fleet">
        <h3>Fleet</h3>
       <p>A variety of airplanes to meet your specic need</p>
+      <div id="fleet_planes_container">
+        <div className="fleet_content_divs_divs">
+          <div className="fleet_content_divs">KA350</div>
+          <div className="fleet_pics"></div>
+        </div>
+
+        <div className="fleet_content_divs_divs">
+          <div className="fleet_pics"></div>
+          <div className="fleet_content_divs">TBM900</div>
+        </div>
+
+        <div className="fleet_content_divs_divs">
+          <div className="fleet_content_divs">C300</div>
+          <div className="fleet_pics"></div>
+        </div>
+
+        <div className="fleet_content_divs_divs">
+          <div className="fleet_pics"></div>
+          <div className="fleet_content_divs">C650</div>
+        </div>
+
+        <div className="fleet_content_divs_divs">
+          <div className="fleet_content_divs">MD80</div>
+          <div className="fleet_pics"></div>
+        </div>
+
+        <div className="fleet_content_divs_divs">
+          <div className="fleet_pics"></div>
+          <div className="fleet_content_divs">737</div>
+        </div>
+
+        <div className="fleet_content_divs_divs">
+          <div className="fleet_content_divs">767</div>
+          <div className="fleet_pics"></div>
+        </div>
+      </div>
     </div>
   );
 };
