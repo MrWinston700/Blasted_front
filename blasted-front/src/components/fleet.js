9c3805ebf8c69d93cf52985157270ff9dd47c659
MrWinston700 <onestarrecruit@gmail.com>
2021-12-17 22:35:14 -0500

    code/style: work started on fleet slideshow. image shows but slideshow not functional

--- blasted-front/src/components/fleet.js
+++ blasted-front/src/components/fleet.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 // for now, just add some photos and make it look good
 import king_air from '../images/king_air.jpg'
 
-const Fleet = () => {
+class Fleet extends Component {
+
+  state = {slideIndex: 1};
+
+    componentDidMount() {
+      this.showSlides(this.state.slideIndex);
+      console.log("I executed");
+    }
+
+    showSlides(n) {
+    let i = 0;
+    const slides = document.getElementsByClassName("mySlides");
+    console.log(slides);
+    console.log(slides.length);
+    const dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.setState({slideIndex: 1})}    
+    if (n < 1) {this.setState({slideIndex: slides.length})}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.state.slideIndex-1].style.display = "block";  
+    dots[this.state.slideIndex-1].className += " active";
+  }
+
+   plusSlides(n) {
+    this.showSlides(this.state.slideIndex += n);
+  }
+  
+  currentSlide(n) {
+    this.showSlides(this.state.slideIndex = n);
+  }
+
+  render() {
   return (
+    
     <div id="fleet">
        <h3>Fleet</h3>
       <p>A variety of airplanes to meet your specic need</p>
@@ -25,7 +62,32 @@ const Fleet = () => {
       
 
         <div className="fleet_content_divs_divs">
-          <div className="fleet_pics_divs"></div>
+          <div className="fleet_pics_divs">
+          <div className="slideshow-container">
+
+            <div className="mySlides fade">
+              <img src={king_air} className="fleet_pics"></img>
+            </div>
+
+            <div className="mySlides fade">
+              <img src={king_air} ></img>
+            </div>
+
+            <div className="mySlides fade">
+              <img src={king_air} ></img>
+            </div>
+
+            <a className="prev" >&#10095;</a>
+            <a className="next">&#10095;</a>
+
+            </div>
+
+            <div style={{textAlign : 'center'}}>
+              <span className="dot" ></span> 
+              <span className="dot" ></span> 
+              <span className="dot" ></span> 
+            </div>
+          </div>
           <div className="fleet_content_divs">
             
             <h2>TBM900</h2>
@@ -123,6 +185,7 @@ const Fleet = () => {
       </div>
     </div>
   );
+  }
 };
 
 export default Fleet;
